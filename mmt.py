13169534c16b9e5d7fe540b63448107d34888b55
Samuel Lelièvre <samuel.lelievre@gmail.com>
2016-04-07 13:17:19 +0200

    Start annotating Groups in mmt.py

--- mmt.py
+++ mmt.py
@@ -318,6 +318,70 @@ class AdditiveMagmas:
 class AdditiveSemigroups:
     pass
 
+@semantic(mmt="Group", variant="multiplicative", module_name="sage.categories.groups")
+class Groups:
+
+    class ParentMethods:
+
+        @semantic(gap="IsAbelian")
+        def is_abelian(self):
+            pass
+
+        @semantic(gap="GeneratorsOfGroup", codomain="list_of_self") # TODO: tuple_of_self
+        def group_generators(self):
+            pass
+
+        @semantic(gap="\/")
+        def __truediv__(self, relators):
+            r"""
+            Return the quotient group of self by list of relations or relators
+
+            TODO:
+
+            - also accept list of relations as couples of elements,
+              like semigroup quotients do.
+
+            EXAMPLES:
+
+            We define `ZZ^2` as a quotient of the free group
+            on two generators::
+
+                sage: sys.path.insert(0, "./")
+                sage: from mygap import mygap
+                sage: F = mygap.FreeGroup("a", "b")
+                sage: a, b = F.group_generators()
+                sage: G = F / [ a * b * a^-1 * b^-1 ]
+                sage: G
+                <fp group of size infinity on the generators [ a, b ]>
+                sage: a, b = G.group_generators()
+                sage: a * b * a^-1 * b^-1
+                a*b*a^-1*b^-1
+
+            Here, equality testing works well::
+
+                sage: a * b * a^-1 * b^-1 == G.one()
+                True
+
+            We define a Baumslag-Solitar group::
+
+                sage: a, b = F.group_generators()
+                sage: G = F / [ a * b * a^-1 * b^-2 ]
+                sage: G
+                <fp group of size infinity on the generators [ a, b ]>
+                sage: a, b = G.group_generators()
+                sage: a * b * a^-1 * b^-2
+                a*b*a^-1*b^-2
+
+            Here, equality testing starts an infinite loop where GAP is trying
+            to make the rewriting system confluent (a better implementation
+            would alternate between making the rewriting system more confluent
+            and trying to answer the equality question -- this is a known
+            limitation of GAP's implementation of the Knuth-Bendix procedure)::
+
+                sage: a * b * a^-1 * b^-2 == G.one() # not tested
+            """
+            return self._wrap( self.gap() / libgap([x.gap() for x in relators]) )
+
 @semantic(mmt="Ring")
 class Rings:
     pass

65bdd1f142866d7982490579a9cd4a36657dd3db
Samuel Lelièvre <samuel.lelievre@gmail.com>
2016-04-08 01:24:35 +0200

    Add semantics for semigroups in mmt.py

--- mmt.py
+++ mmt.py
@@ -299,24 +299,102 @@ class AdditiveMagmas:
             @semantic(mmt="neutral", gap="Zero", codomain="self")
             @abstract_method
             def zero(self):
+                # Generates automatically in the XXX.GAP category
+                # def zero(self): return self(self.gap().Zero())
                 pass
-
-            # Generates automatically in the XXX.GAP category
-            # def zero(self): return self(self.gap().Zero())
 
         class ElementMethods:
             @semantic(gap=r"\-", codomain="parent")
             @abstract_method
             def _sub_(self, other):
+                # Generates automatically
+                # def _sub_(self,other): return self(gap.Subtract(self.gap(), other.gap()))
                 pass
 
-            # Generates automatically
-            # def _sub_(self,other): return self(gap.Subtract(self.gap(), other.gap()))
+    class AdditiveInverse:
+        class ElementMethods:
+            @semantic(gap="AdditiveInverse", codomain="parent")
+            @abstract_method
+            def _neg_(self):
+                # Generates automatically
+                # def _neg_(self): return self.parent()(self.gap().AdditiveInverse())
+                pass
 
+@semantic(mmt="Magma", variant="multiplicative", module_name="sage.categories.magmas")
+class Magmas:
+    class ElementMethods:
+        @semantic(mmt="*", gap=r"\*", codomain="parent") #, operator="*"
+        @abstract_method
+        def _mul_(self, other):
+            pass
 
-@semantic(mmt="Semigroup")
+@semantic(mmt="Semigroup", variant="additive", module_name="sage.categories.additive_semigroups")
 class AdditiveSemigroups:
     pass
+
+@semantic(mmt="Semigroup", variant="multiplicative", module_name="sage.categories.semigroups")
+class Semigroups:
+    class ParentMethods:
+        @semantic(gap="GeneratorsOfSemigroup", codomain="list_of_self") # TODO: tuple_of_self
+        def semigroup_generators(self):
+            pass
+
+        @semantic(gap="\/")
+        def __truediv__(self, relations):
+            pass
+
+        @semantic(gap="IsLTrivial")
+        def is_l_trivial(self):
+            pass
+
+        @semantic(gap="IsRTrivial")
+        def is_r_trivial(self):
+            pass
+
+        @semantic(gap="IsDTrivial")
+        def is_d_trivial(self):
+            pass
+
+    @semantic()
+    class Finite:
+        class ParentMethods:
+            @semantic(gap="JClasses")
+            def j_classes(self):
+                pass
+
+            @semantic(gap="LClasses")
+            def l_classes(self):
+                pass
+
+            @semantic(gap="RClasses")
+            def r_classes(self):
+                pass
+
+            @semantic(gap="StructureDescriptionMaximalSubgroups")
+            def structure_description_maximal_subgroups(self):
+                pass
+
+            @semantic(gap="StructureDescriptionSchutzenbergerGroups")
+            def structure_description_schutzenberger_groups(self):
+                pass
+
+            @semantic(gap="IsomorphismTransformationSemigroup")
+            def isomorphism_transformation_semigroup(self):
+                pass
+
+    @semantic()
+    class Unital:
+        class ParentMethods:
+            @semantic(gap="GeneratorsOfMonoid", codomain="list_of_self") # TODO: tuple_of_self
+            def monoid_generators(self):
+                pass
+
+    @semantic()
+    class Finite:
+        class ParentMethods:
+            @semantic(gap="IsomorphismTransformationMonoid")
+            def isomorphism_transformation_monoid(self):
+                pass
 
 @semantic(mmt="Group", variant="multiplicative", module_name="sage.categories.groups")
 class Groups:
@@ -333,54 +411,7 @@ class Groups:
 
         @semantic(gap="\/")
         def __truediv__(self, relators):
-            r"""
-            Return the quotient group of self by list of relations or relators
-
-            TODO:
-
-            - also accept list of relations as couples of elements,
-              like semigroup quotients do.
-
-            EXAMPLES:
-
-            We define `ZZ^2` as a quotient of the free group
-            on two generators::
-
-                sage: sys.path.insert(0, "./")
-                sage: from mygap import mygap
-                sage: F = mygap.FreeGroup("a", "b")
-                sage: a, b = F.group_generators()
-                sage: G = F / [ a * b * a^-1 * b^-1 ]
-                sage: G
-                <fp group of size infinity on the generators [ a, b ]>
-                sage: a, b = G.group_generators()
-                sage: a * b * a^-1 * b^-1
-                a*b*a^-1*b^-1
-
-            Here, equality testing works well::
-
-                sage: a * b * a^-1 * b^-1 == G.one()
-                True
-
-            We define a Baumslag-Solitar group::
-
-                sage: a, b = F.group_generators()
-                sage: G = F / [ a * b * a^-1 * b^-2 ]
-                sage: G
-                <fp group of size infinity on the generators [ a, b ]>
-                sage: a, b = G.group_generators()
-                sage: a * b * a^-1 * b^-2
-                a*b*a^-1*b^-2
-
-            Here, equality testing starts an infinite loop where GAP is trying
-            to make the rewriting system confluent (a better implementation
-            would alternate between making the rewriting system more confluent
-            and trying to answer the equality question -- this is a known
-            limitation of GAP's implementation of the Knuth-Bendix procedure)::
-
-                sage: a * b * a^-1 * b^-2 == G.one() # not tested
-            """
-            return self._wrap( self.gap() / libgap([x.gap() for x in relators]) )
+            pass
 
 @semantic(mmt="Ring")
 class Rings:
